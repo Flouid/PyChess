25cc46d200d57e2f1a168333adba10c6222ff534
Flouid <louiskeith99@gmail.com>
2022-06-17 10:04:06 -0400

    pawns (minus en passant) work

--- main.py
+++ main.py
@@ -245,28 +245,48 @@ class ChessUI(QWidget):
 
                     # generate pawn moves
                     if piece.role == 'pawn':
-
-                        # pawn move direction is determined by color
-                        if piece.light:
-                            # range is negative for light since it moves up the board
-                            max_range = [-1 * n for n in range(1, 2 + (r == 6))]
-                        else:
-                            # range is positive for dark since it moves down the board
-                            max_range = list(range(1, 2 + (r == 1)))
-
-                        # iterate down the range of the pawn
-                        for n in max_range:
-                            # calculate the position of the landing tile
-                            target = Position(r + n, c)
-                            # if the current tile does not contain an allied piece, it is a valid move
-                            if not target.does_share_color(self.board, self.isLightTurn):
-                                moves.append(Move(begin, target))
-                            # an allied piece cannot be jumped over, so stop searching
-                            else:
-                                break
+                        moves.extend(self.generate_pawn_moves(begin, piece))
 
         self.moves = moves
 
+    def generate_pawn_moves(self, begin, piece):
+        """Generates all of the moves possible for the current piece given that it is a pawn"""
+        moves = []
+
+        # pawn move direction is determined by color
+        if piece.light:
+            # range is negative for light since it moves up the board
+            max_range = [-1 * n for n in range(1, 2 + (begin.r == 6))]
+        else:
+            # range is positive for dark since it moves down the board
+            max_range = list(range(1, 2 + (begin.r == 1)))
+
+        # iterate down the range of the pawn
+        for n in max_range:
+
+            # calculate potential attacks
+            if abs(n) == 1:
+                # account for the west boundary of the board
+                if begin.c > 0:
+                    west = Position(begin.r + n, begin.c - 1)
+                    if west.contains_enemy_piece(self.board, self.isLightTurn):
+                        moves.append(Move(begin, west))
+                # account for the east boundary of the board
+                if begin.c < 7:
+                    east = Position(begin.r + n, begin.c + 1)
+                    if east.contains_enemy_piece(self.board, self.isLightTurn):
+                        moves.append(Move(begin, east))
+
+            # calculate the position of the landing tile
+            target = Position(begin.r + n, begin.c)
+            # if the current tile does not contain an allied piece, it is a valid move
+            if not target.does_share_color(self.board, self.isLightTurn):
+                moves.append(Move(begin, target))
+            # an allied piece cannot be jumped over, so stop searching
+            else:
+                break
+
+        return moves
 
 def main():
     app = QApplication(sys.argv)
--- position.py
+++ position.py
@@ -15,6 +15,10 @@ class Position:
     def __str__(self):
         return f'({self.r}, {self.c})'
 
+    def contains_enemy_piece(self, board, isLight):
+        """Checks if a tile on a given board contains an enemy piece to a given color"""
+        return board.board[self.r, self.c] is not None and board.board[self.r, self.c].light != isLight
+
     def does_share_color(self, board, isLight):
         """Checks if a tile on a given board contains a piece of a given color"""
         return board.board[self.r, self.c] is not None and board.board[self.r, self.c].light == isLight
