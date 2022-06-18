00e52f67b55a543e2e175008a9b3fc6dd9806136
Flouid <louiskeith99@gmail.com>
2022-06-18 13:52:36 -0400

    reverted previous change

--- move.py
+++ move.py
@@ -2,19 +2,13 @@ from dataclasses import dataclass
 from position import Position
 
 
+@dataclass
 class Move:
     """A class for representing a single move as a pair of positions.
-    Tracks whether a move is an en passant capture or a castle for the rare cases that matters"""
+    Tracks whether a move is an en passant capture for the rare case that matters"""
     begin: Position
     target: Position
-    is_ep_cap: bool
-    is_castle: bool
-
-    def __init__(self, begin, target, is_ep_cap=False, is_castle=False):
-        self.begin = begin
-        self.target = target
-        self.is_ep_cap = is_ep_cap
-        self.is_castle = is_castle
+    is_ep_cap: bool = False
 
     def __eq__(self, __o):
         return self.begin == __o.begin and self.target == __o.target
